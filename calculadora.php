ccaee917b2ed6cf36ef56f3604c68479d88353f7
2DAW04 <2DAW04@esvirgua.es>
2021-10-08 13:30:16 +0200

    Primer commit

---
+++ calculadora.php
@@ -0,0 +1,16 @@
+<?php
+    class Calculadora {
+        function suma($num1,$num2){
+            return $num1+$num2;
+        }
+        function resta($num1, $num2) {
+            return $num1-$num2;
+        }
+        function multiplica($num1, $num2) {
+            return $num1*$num2;
+        }
+        function divide($num1, $num2) {
+            return $num1/$num2;
+        }
+    }
+?>
---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Calculadora</title>
+    </head>
+    <body>
+        <form action="#" method="post">
+            Valor 1: 
+            <input type="number" name="valor1" id="">
+            Valor 2:
+            <input type="number" name="valor2" id="">
+
+            <select name="valores" id="">
+                <option value="sumar">Sumar</option>
+                <option value="restar">Restar</option>
+                <option value="multiplicar">Multiplicar</option>
+                <option value="dividir">Dividir</option>
+            </select>
+
+            <input type="submit" value="Enviar" name="enviar[]">
+
+        </form>
+    </body>
+</html>
+
+<?php
+    //Autor: Sergio Matamoros Delgado
+    //Fecha: 8-10-21
+    //Licencia: 
+
+    //Importamos librerias
+    include("calculadora.php");
+
+    class Principal {
+        function __construct($num1,$num2,$tipo)
+        {
+            $this->inicio($num1,$num2,$tipo);
+        }
+        function inicio($num1,$num2,$tipo) {
+
+            //Def obj
+            $calculadora = new Calculadora();
+            $resultado = 0;
+
+            //Si has elegido la opción suma o resta...
+            if($_POST["valores"] == "sumar") {
+                $resultado = $calculadora->suma($num1,$num2);
+            } else if($_POST["valores"] == "restar"){
+                $resultado = $calculadora->resta($num1,$num2);
+            }
+            else if($_POST["valores"] == "multiplicar"){
+                $resultado = $calculadora->multiplica($num1,$num2);
+            }
+            else if($_POST["valores"] == "dividir"){
+                $resultado = $calculadora->divide($num1,$num2);
+            }
+
+            echo "<br><br>Resultado: ". $resultado;
+
+            
+        }
+    }
+
+    if(isset($_POST["enviar"]))
+        new Principal($_POST["valor1"],$_POST["valor2"],$_POST["valores"]);
+?>
